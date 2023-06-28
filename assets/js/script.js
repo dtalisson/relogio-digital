5043fc00d6a4e3617f9fa7fdefb5e6289f4e5f27
dtalisson <darlan-talisson@outlook.com>
2023-06-28 11:13:21 -0300

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+let horas = document.querySelector('#horas') 
+let minutos = document.querySelector('#minutos')
+let segundos = document.querySelector('#segundos')
+ // pegando as váriaveis 
+
+const relogio = setInterval(function time() {  // function onde executa certos códigos em determinados intervalos 
+    let dateToday = new Date()  // cria uma variavel onde vai pegar as informações de hoje
+    let hr = dateToday.getHours() // cria uma váriavel onde pega a hora 
+    let min = dateToday.getMinutes()  // cria uma váriavel que pega os minutos 
+    let seconds = dateToday.getSeconds() // cria uma váriavel que pega os segundos 
+
+    if ( hr < 10) hr = '0'+ hr
+    if ( min < 10) min = '0'+ min
+    if ( seconds < 10) seconds = '0'+ seconds
+  // condições que caso chegue em 10 e não tenha o '0', execute-os:
+    horas.innerHTML = hr;  // usando a váriavel onde pegou a hora e inserindo no bloco
+    minutos.innerHTML = min; // usando a váriavel onde pegou os minutos e inserindo no bloco
+    segundos.innerHTML = seconds // usando a váriavel onde pegou os segundos e inserindo no bloco
+})
+
